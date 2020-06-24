dab7476d670d4eb6e55f52f35c32f6cc29653a4d
keshavsavva <keshav.s.avva@gmail.com>
2020-06-24 17:21:34 -0400

    added basic characterCard

---
+++ src/components/CharacterCard/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Paper from '@material-ui/core/Paper';
+
+export class CharacterCard extends Component {
+    render() {
+        return (
+            <div className="characterCard">
+                
+            </div>
+        )
+    }
+}
+
+export default CharacterCard
